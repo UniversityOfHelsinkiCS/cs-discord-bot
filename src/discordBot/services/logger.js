c10f231c6d95fd0834cf8676f126bd1d0f1ad083
Jami Kousa <jami.kousa@helsinki.fi>
2022-04-30 13:02:50 +0300

    refactor logger

--- src/discordBot/services/logger.js
+++ src/discordBot/services/logger.js
@@ -1,25 +1,20 @@
 const { createLogger, format, transports } = require("winston");
-const { PapertrailConnection, PapertrailTransport } = require("winston-papertrail");
+const {
+  PapertrailConnection,
+  PapertrailTransport,
+} = require("winston-papertrail");
 
 let logger;
 
-if (process.env.NODE_ENV !== "test") {
-  const winstonPapertrail = new PapertrailConnection({
-    host: process.env.PAPERTRAIL_URL,
-    port: 10737,
-  });
+const setupLogger = () => {
+  if (process.env.NODE_ENV === "test") return;
 
-
-  const paperTrailTransport = new PapertrailTransport(winstonPapertrail);
-
-
-  const errorStackTracerFormat = format(info => {
+  const errorStackTracerFormat = format((info) => {
     if (info.stack) {
       info.message = `${info.message} ${info.stack}`;
     }
     return info;
   });
-
 
   logger = createLogger({
     format: format.combine(
@@ -27,61 +22,70 @@ if (process.env.NODE_ENV !== "test") {
       format.splat(),
       format.colorize(),
       errorStackTracerFormat(),
-      format.simple(),
+      format.simple()
     ),
-    transports: [paperTrailTransport],
+    transports: [],
   });
 
-  if (process.env.NODE_ENV === "development") {
-    logger.add(new transports.Console({
+  logger.add(
+    new transports.Console({
       format: format.simple(),
-    }));
-    logger.remove(paperTrailTransport);
-  }
-
-  logger.rejections.handle(
-    paperTrailTransport,
+    })
   );
 
-  logger.exceptions.handle(
-    paperTrailTransport,
-  );
+  if (!process.env.PAPERTRAIL_URL) return;
 
-  winstonPapertrail.on("connect", function() {
+  const winstonPapertrail = new PapertrailConnection({
+    host: process.env.PAPERTRAIL_URL,
+    port: 10737,
+  });
+
+  const paperTrailTransport = new PapertrailTransport(winstonPapertrail);
+
+  logger.add(paperTrailTransport);
+
+  logger.rejections.handle(paperTrailTransport);
+
+  logger.exceptions.handle(paperTrailTransport);
+
+  winstonPapertrail.on("connect", function () {
     logger.info("Logger connected to Papertrail");
   });
-}
+};
+
+setupLogger();
 
 const logError = (error) => {
-  if (logger) {
-    logger.error(error);
-  }
+  logger.error(error);
 };
 
 const logInfo = (message) => {
-  if (logger) {
-    logger.info(message);
-  }
+  logger.info(message);
 };
 
 const logInteractionError = (error, client, interaction) => {
-  if (logger) {
-    const member = client.guild.members.cache.get(interaction.member.user.id);
-    const channel = client.guild.channels.cache.get(interaction.channelId);
-    const msg = `ERROR DETECTED!\nMember: ${member.displayName}\nCommand: ${interaction.commandName}\nChannel: ${channel.name}}`;
-    logger.error(msg);
-    logger.error(error);
-  }
+  const member = client.guild.members.cache.get(interaction.member.user.id);
+  const channel = client.guild.channels.cache.get(interaction.channelId);
+  const msg = `ERROR DETECTED!\nMember: ${member.displayName}\nCommand: ${interaction.commandName}\nChannel: ${channel.name}}`;
+  logger.error(msg);
+  logger.error(error);
 };
 
-const logNoInteractionError = async (telegramId, member, channel, client, error) => {
-  if (logger) {
-    const msg = `ERROR DETECTED!\nMember: ${member}\nChannel: ${channel}`;
-    logger.error(msg);
-    logger.error(error);
-  }
+const logNoInteractionError = async (
+  telegramId,
+  member,
+  channel,
+  client,
+  error
+) => {
+  const msg = `ERROR DETECTED!\nMember: ${member}\nChannel: ${channel}`;
+  logger.error(msg);
+  logger.error(error);
 };
 
 module.exports = {
-  logError, logInteractionError, logNoInteractionError, logInfo,
+  logError,
+  logInteractionError,
+  logNoInteractionError,
+  logInfo,
 };
