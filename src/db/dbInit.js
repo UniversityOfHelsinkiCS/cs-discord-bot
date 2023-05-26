c1dfc025ac87e416fd583659bda71a8cf6e3929b
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-05-26 13:57:38 +0300

    Update database connection method

--- src/db/dbInit.js
+++ src/db/dbInit.js
@@ -1,17 +1,9 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
-const password = process.env.POSTGRES_PASSWORD;
 
-let username = process.env.POSTGRES_USERNAME;
-if (!username) {
-  username = "postgres";
-}
+const dbUrl = process.env.DATABASE_URL;
 
-const sequelize = new Sequelize("postgres", username, password, {
-  dialect: "postgres",
-  host: process.env.DB_HOST || "localhost",
-  logging: false,
-});
+const sequelize = new Sequelize(dbUrl, { logging: false });
 
 const Course = require("./models/Course")(sequelize, Sequelize.DataTypes);
 const Channel = require("./models/Channel")(sequelize, Sequelize.DataTypes);
