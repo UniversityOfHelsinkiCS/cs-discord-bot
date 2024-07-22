5a3a69c93064f28135526a12fa0e8bc798f92efd
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-07-22 13:17:08 +0300

    webhook-proxy: Also forward query passed in the request

--- src/server/routes/webhookProxy.js
+++ src/server/routes/webhookProxy.js
@@ -8,14 +8,17 @@ router.get("/:webhookId", async (req, res) => {
     const webhookUrl = `${process.env.DISCORD_WEBHOOK_URL}/${req.params.webhookId}/${process.env.DISCORD_WEBHOOK_TOKEN}`
     const webhookData = req.body
     try {
-      await axios.post(webhookUrl, webhookData)
+      await axios.post(webhookUrl, webhookData, { params: req.query })
       res.status(200).send("Webhook sent successfully")
+      return
     } catch (error) {
       console.error(error)
       res.status(500).send("Internal server error")
+      return
     }
   } else {
     res.status(405).send("Method not allowed")
+    return
   }
 })
 
