dafa45e8ceaf60a188573ad7baa94bbd7e556a0f
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-07-22 14:10:39 +0300

    Fix webhook proxy endpoint

--- src/server/routes/webhookProxy.js
+++ src/server/routes/webhookProxy.js
@@ -2,25 +2,16 @@ require("dotenv").config()
 const axios = require("axios")
 const router = require("express").Router()
 
-router.get("/:webhookId", async (req, res) => {
-  console.log(req)
-  // only allow POST
-  if (req.method === "POST") {
-    const webhookUrl = `${process.env.DISCORD_WEBHOOK_URL}/${req.params.webhookId}/${process.env.DISCORD_WEBHOOK_TOKEN}`
-    const webhookData = req.body
-    try {
-      console.log('Sending webhook', webhookUrl, webhookData)
-      await axios.post(webhookUrl, webhookData, { params: req.query })
-      console.log('Webhook sent successfully')
-      res.status(200).send("Webhook sent successfully")
-      return
-    } catch (error) {
-      console.error(error)
-      res.status(500).send("Internal server error")
-      return
-    }
-  } else {
-    res.status(405).send("Method not allowed")
+router.post("/:webhookId", async (req, res) => {
+  const webhookUrl = `${process.env.DISCORD_WEBHOOK_URL}/${req.params.webhookId}/${process.env.DISCORD_WEBHOOK_TOKEN}`
+  const webhookData = req.body
+  try {
+    await axios.post(webhookUrl, webhookData, { params: req.query })
+    res.status(200).send("Webhook sent successfully")
+    return
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("Internal server error")
     return
   }
 })
