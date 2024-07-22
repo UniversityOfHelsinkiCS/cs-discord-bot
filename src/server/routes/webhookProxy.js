6afd8fbdb72ced2c38f7e6983c40940924ae9f49
Tereshchenko Aleksandr <aleteres@lx3-500-30148.ad.helsinki.fi>
2024-07-22 13:54:27 +0300

    Add debug logging

--- src/server/routes/webhookProxy.js
+++ src/server/routes/webhookProxy.js
@@ -3,12 +3,15 @@ const axios = require("axios")
 const router = require("express").Router()
 
 router.get("/:webhookId", async (req, res) => {
+  console.log(req)
   // only allow POST
   if (req.method === "POST") {
     const webhookUrl = `${process.env.DISCORD_WEBHOOK_URL}/${req.params.webhookId}/${process.env.DISCORD_WEBHOOK_TOKEN}`
     const webhookData = req.body
     try {
+      console.log('Sending webhook', webhookUrl, webhookData)
       await axios.post(webhookUrl, webhookData, { params: req.query })
+      console.log('Webhook sent successfully')
       res.status(200).send("Webhook sent successfully")
       return
     } catch (error) {
